183e032922c99e6249293a951397ab8f69607ba6
neoge <neoge@DESKTOP-UFJULB5>
2018-12-10 09:53:05 +0900

    초기 커밋.

---
+++ WinAppRemote/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WinAppRemote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.txtBxAppRunRemote = new System.Windows.Forms.TextBox();
+            this.txtBxFilename = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAppRun = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnAppRunRemote = new System.Windows.Forms.Button();
+            this.btnSnapshotRevert = new System.Windows.Forms.Button();
+            this.lblIPAddr = new System.Windows.Forms.Label();
+            this.txtBxIPAddr = new System.Windows.Forms.TextBox();
+            this.strConvert = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBxStrPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnFileExists = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(75, 161);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBxAppRunRemote
+            // 
+            this.txtBxAppRunRemote.Location = new System.Drawing.Point(129, 92);
+            this.txtBxAppRunRemote.Name = "txtBxAppRunRemote";
+            this.txtBxAppRunRemote.Size = new System.Drawing.Size(100, 20);
+            this.txtBxAppRunRemote.TabIndex = 1;
+            // 
+            // txtBxFilename
+            // 
+            this.txtBxFilename.Location = new System.Drawing.Point(129, 64);
+            this.txtBxFilename.Name = "txtBxFilename";
+            this.txtBxFilename.Size = new System.Drawing.Size(100, 20);
+            this.txtBxFilename.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File Name";
+            // 
+            // lblAppRun
+            // 
+            this.lblAppRun.AutoSize = true;
+            this.lblAppRun.Location = new System.Drawing.Point(72, 95);
+            this.lblAppRun.Name = "lblAppRun";
+            this.lblAppRun.Size = new System.Drawing.Size(44, 13);
+            this.lblAppRun.TabIndex = 4;
+            this.lblAppRun.Text = "App run";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(75, 201);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 5;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnAppRunRemote
+            // 
+            this.btnAppRunRemote.Location = new System.Drawing.Point(184, 161);
+            this.btnAppRunRemote.Name = "btnAppRunRemote";
+            this.btnAppRunRemote.Size = new System.Drawing.Size(100, 23);
+            this.btnAppRunRemote.TabIndex = 6;
+            this.btnAppRunRemote.Text = "Run app";
+            this.btnAppRunRemote.UseVisualStyleBackColor = true;
+            this.btnAppRunRemote.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnSnapshotRevert
+            // 
+            this.btnSnapshotRevert.Location = new System.Drawing.Point(184, 201);
+            this.btnSnapshotRevert.Name = "btnSnapshotRevert";
+            this.btnSnapshotRevert.Size = new System.Drawing.Size(100, 23);
+            this.btnSnapshotRevert.TabIndex = 7;
+            this.btnSnapshotRevert.Text = "Snapshot revert";
+            this.btnSnapshotRevert.UseVisualStyleBackColor = true;
+            this.btnSnapshotRevert.Click += new System.EventHandler(this.btnSnapshotRevert_Click);
+            // 
+            // lblIPAddr
+            // 
+            this.lblIPAddr.AutoSize = true;
+            this.lblIPAddr.Location = new System.Drawing.Point(72, 121);
+            this.lblIPAddr.Name = "lblIPAddr";
+            this.lblIPAddr.Size = new System.Drawing.Size(52, 13);
+            this.lblIPAddr.TabIndex = 9;
+            this.lblIPAddr.Text = "IP ( host )";
+            // 
+            // txtBxIPAddr
+            // 
+            this.txtBxIPAddr.Location = new System.Drawing.Point(129, 118);
+            this.txtBxIPAddr.Name = "txtBxIPAddr";
+            this.txtBxIPAddr.Size = new System.Drawing.Size(100, 20);
+            this.txtBxIPAddr.TabIndex = 8;
+            // 
+            // strConvert
+            // 
+            this.strConvert.Location = new System.Drawing.Point(75, 241);
+            this.strConvert.Name = "strConvert";
+            this.strConvert.Size = new System.Drawing.Size(75, 27);
+            this.strConvert.TabIndex = 10;
+            this.strConvert.Text = "String convert";
+            this.strConvert.UseVisualStyleBackColor = true;
+            this.strConvert.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Str Path";
+            // 
+            // txtBxStrPath
+            // 
+            this.txtBxStrPath.Location = new System.Drawing.Point(129, 38);
+            this.txtBxStrPath.Name = "txtBxStrPath";
+            this.txtBxStrPath.Size = new System.Drawing.Size(100, 20);
+            this.txtBxStrPath.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 27);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Delete remotely";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_3);
+            // 
+            // btnFileExists
+            // 
+            this.btnFileExists.Location = new System.Drawing.Point(304, 161);
+            this.btnFileExists.Name = "btnFileExists";
+            this.btnFileExists.Size = new System.Drawing.Size(100, 23);
+            this.btnFileExists.TabIndex = 14;
+            this.btnFileExists.Text = "File exists";
+            this.btnFileExists.UseVisualStyleBackColor = true;
+            this.btnFileExists.Click += new System.EventHandler(this.btnFileExists_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 321);
+            this.Controls.Add(this.btnFileExists);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBxStrPath);
+            this.Controls.Add(this.strConvert);
+            this.Controls.Add(this.lblIPAddr);
+            this.Controls.Add(this.txtBxIPAddr);
+            this.Controls.Add(this.btnSnapshotRevert);
+            this.Controls.Add(this.btnAppRunRemote);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.lblAppRun);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBxFilename);
+            this.Controls.Add(this.txtBxAppRunRemote);
+            this.Controls.Add(this.btnUpload);
+            this.Name = "Form1";
+            this.Text = "Copy File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.TextBox txtBxAppRunRemote;
+        private System.Windows.Forms.TextBox txtBxFilename;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblAppRun;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnAppRunRemote;
+        private System.Windows.Forms.Button btnSnapshotRevert;
+        private System.Windows.Forms.Label lblIPAddr;
+        private System.Windows.Forms.TextBox txtBxIPAddr;
+        private System.Windows.Forms.Button strConvert;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBxStrPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnFileExists;
+    }
+}
+
---
+++ WinAppRemote/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Principal;
+using System.Security.Permissions;
+using WinSCP;
+using System.Net;
+using System.Management;
+using System.Text.RegularExpressions;
+using System.IO;
+using XenAPI;
+using System.Threading;
+
+
+namespace WinAppRemote
+{
+    public partial class Form1 : Form
+    {
+        //[DllImport("advapi32.DLL", SetLastError = true)]
+        //public static extern int LogonUser(string lpszUsername, string lpszDomain, string lpszPassword, int dwLogonType, int dwLogonProvider, ref IntPtr phToken);
+        string fingerprint = string.Empty;
+        //public event Action<IXenConnection> CachePopulated;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SessionOptions sessionOptions = new SessionOptions
+        {
+            Protocol = Protocol.Sftp,
+            HostName = "192.168.1.179",
+            UserName = "administrator",
+            Password = "1311",
+            //SshHostKeyFingerprint = "ssh-ed25519 256 28:c2:b9:05:c1:04:e9:83:b9:65:8b:66:e4:dd:a1:53"
+        };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            using (WinSCP.Session session = new WinSCP.Session())
+            {
+                fingerprint = session.ScanFingerprint(sessionOptions, "MD5");
+                sessionOptions.SshHostKeyFingerprint = fingerprint;
+                session.Open(sessionOptions);
+                TransferOptions transferOptions = new TransferOptions();
+                transferOptions.TransferMode = TransferMode.Binary;
+                string sourcePath = @"c:\test\setup-x86_64.exe";
+                string destinationPath = @"/c:/MalCode/";
+                string textBxInput = this.txtBxFilename.Text;
+                if (!string.IsNullOrEmpty(textBxInput))
+                {
+                    sourcePath = @"c:\test\"+textBxInput;
+                }
+
+
+                TransferOperationResult transferResult;
+                transferResult = session.PutFiles(sourcePath, destinationPath, false, transferOptions);
+
+                transferResult.Check();
+
+                foreach(TransferEventArgs transfer in transferResult.Transfers)
+                {
+                    System.Console.WriteLine("Upload of {0} succeeded ", transfer.FileName);
+                }
+            }
+            
+
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            
+            using (WinSCP.Session session = new WinSCP.Session())
+            {
+                fingerprint = session.ScanFingerprint(sessionOptions, "MD5");
+                sessionOptions.SshHostKeyFingerprint = fingerprint;
+                session.Open(sessionOptions);
+                TransferOptions transferOptions = new TransferOptions();
+                transferOptions.TransferMode = TransferMode.Binary;
+
+                TransferOperationResult transferResult;
+                transferResult = session.GetFiles(@"/c:/MalCode/HiWinForm.exe", @"c:\test\", false, transferOptions);
+
+                transferResult.Check();
+
+                foreach (TransferEventArgs transfer in transferResult.Transfers)
+                {
+                    System.Console.WriteLine("Upload of {0} succeeded ", transfer.FileName);
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            System.Management.ConnectionOptions connOptions =
+            new System.Management.ConnectionOptions();
+
+            connOptions.Impersonation = System.Management.ImpersonationLevel.Impersonate;
+            connOptions.EnablePrivileges = true;
+            connOptions.Username = "Administrator";
+            connOptions.Password = "1311";
+
+            string compName = "chan-PC";
+            System.Management.ManagementScope manScope =
+                new System.Management.ManagementScope(
+                    String.Format(@"\\{0}\ROOT\CIMV2", compName), connOptions);
+            manScope.Connect();
+
+            System.Management.ObjectGetOptions objectGetOptions =
+                new System.Management.ObjectGetOptions();
+
+            System.Management.ManagementPath managementPath =
+                new System.Management.ManagementPath("Win32_Process");
+
+            System.Management.ManagementClass processClass =
+                new System.Management.ManagementClass(manScope, managementPath, objectGetOptions);
+
+            System.Management.ManagementBaseObject inParams =
+                processClass.GetMethodParameters("Create");
+
+            inParams["CommandLine"] = @"c:\MalCode\hiWinForm.exe";
+
+            System.Management.ManagementBaseObject outParams =
+                processClass.InvokeMethod("Create", inParams, null);
+
+            var returnvalue = outParams["returnValue"];
+            if(outParams["returnValue"].ToString().Equals("0"))
+            {
+                MessageBox.Show("Process execution returned " + unchecked((int)outParams["returnValue"]));
+            }
+            else
+            {
+                MessageBox.Show("No type specified " + unchecked((int)outParams["returnValue"]));
+            }
+            
+
+
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="hosts"></param>
+        /// <returns></returns>
+        
+
+
+        private void btnSnapshotRevert_Click(object sender, EventArgs e)
+        {
+            
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)(0xc0 | 0x300 | 0xc00);
+            string ipAddr = string.Empty;
+            if (!string.IsNullOrEmpty(this.txtBxIPAddr.Text))
+            {
+                ipAddr = this.txtBxIPAddr.Text;
+            }
+            else
+            {
+                ipAddr = string.Empty;
+            }
+
+            XenAPI.Session session = new XenAPI.Session(ipAddr, 443);
+            session.login_with_password("root","dpsvl123", "", "XenSDKSample");
+
+            var vmRecords = VM.get_all_records(session);
+            
+            var vmRef = (from KeyValuePair<XenRef<VM>, VM> kvp in vmRecords
+                         let theVm = kvp.Value
+                         where !theVm.is_a_template && !theVm.is_control_domain
+                         && theVm.name_label.ToLower().Contains("Window_7_64_M1".ToLower())
+                         select kvp.Key).FirstOrDefault();
+
+            var vmRefValue = (from KeyValuePair<XenRef<VM>, VM> kvp in vmRecords
+                              let theVm = kvp.Value
+                              where !theVm.is_a_template && !theVm.is_control_domain
+                              && theVm.name_label.ToLower().Contains("window")
+                              select kvp.Value).FirstOrDefault();
+
+            var vmRefSnapShot = (from KeyValuePair<XenRef<VM>, VM> kvp in vmRecords
+                              let theVm = kvp.Value
+                              where !theVm.is_a_template && !theVm.is_control_domain
+                              && theVm.name_label.ToLower().Contains("inita")
+                              select kvp.Value).FirstOrDefault();
+
+            if (vmRef == null)
+            {
+                MessageBox.Show("Cannot find a halted linux VM. Please create one.");
+            }
+
+            VM vm = VM.get_record(session, vmRef);
+            var snapshotRef = VM.get_snapshots(session, vmRef).FirstOrDefault();
+            //var snapshotUUID = VM.get_uuid(session, snapshotRef);
+            //VM.destroy(session, snapshotRef);
+            //MessageBox.Show("Here is the vm I destroyed! ");
+            //var newVM = VM.snapshot(session, vmRef, "inita");
+            //VM vm = VM.get_record(session, newVM);
+
+            //VM.shutdown(session, vmRef);
+            //VM.start(session, vmRef, false, true);
+
+            VM.revert(session, snapshotRef);
+            //MessageBox.Show("The current state " + VM.get_power_state(session, vmRef));
+            VM.resume(session, vmRef, false, true);
+            //VM.start(session, vmRef, false, true);
+            //MessageBox.Show("The current state " + VM.get_power_state(session, vmRef));
+            //MessageBox.Show("Here is the vm I want to revert "+vm.name_label);
+            //MessageBox.Show("Here is the vm I created" + vm.name_label);
+            MessageBox.Show("Here is the vm I rebooted" + vm.name_label);
+            session.logout();
+            //GC.SuppressFinalize(this);
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            string pathData = string.Empty;
+            if(!string.IsNullOrEmpty(this.txtBxStrPath.Text))
+            {
+                pathData = this.txtBxStrPath.Text;
+            }
+
+            string resultedStr = pathData.Replace(@"\", @"/");
+            string ultimateResult = '/' + resultedStr;
+            MessageBox.Show("Here is the ultimate result " + ultimateResult);
+        }
+
+        private void button1_Click_3(object sender, EventArgs e)
+        {
+            using (WinSCP.Session session = new WinSCP.Session())
+            {
+                fingerprint = session.ScanFingerprint(sessionOptions, "MD5");
+                sessionOptions.SshHostKeyFingerprint = fingerprint;
+                session.Open(sessionOptions);
+                TransferOptions transferOptions = new TransferOptions();
+                transferOptions.TransferMode = TransferMode.Binary;
+
+                TransferOperationResult transferResult;
+                session.RemoveFiles(@"/c:/MalCode/WinForm.exe");
+
+                //transferResult.Check();
+
+                //foreach (TransferEventArgs transfer in transferResult.Transfers)
+                //{
+                //    System.Console.WriteLine("Upload of {0} succeeded ", transfer.FileName);
+                //}
+            }
+        }
+
+        private void btnFileExists_Click(object sender, EventArgs e)
+        {
+            using(WinSCP.Session session = new WinSCP.Session())
+            {
+                fingerprint = session.ScanFingerprint(sessionOptions, "MD5");
+                sessionOptions.SshHostKeyFingerprint = fingerprint;
+                session.Open(sessionOptions);
+
+                TransferOptions transferOptions = new TransferOptions();
+                transferOptions.TransferMode = TransferMode.Binary;
+
+                TransferOperationResult transferResult;
+                if (session.FileExists(@"/c:/sbmon_complete"))
+                {
+                    MessageBox.Show("File {0} exists", @"/c:/sbmon_complete");
+
+                    
+                }
+                else
+                {
+                    MessageBox.Show("File does not exist");
+                }
+            }
+        }
+    }
+
+
+
+    
+}
